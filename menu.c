76969c4d7fb84b4fafa8967abffc26d4790aa95d
Cyril10 <95445932+Cyril10@users.noreply.github.com>
2021-12-16 00:16:14 +0100

    version 1 de l'application(decrit dans le fichier texte version)

---
+++ menu.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "gestion_ingredient.h"
+#include "gestion_recette.h"
+#include "new_string.h"
+
+void menu(noeud_ingredient* n_i, noeud_recette* n_r){
+	char temp1='\0';
+	char *temp2;
+	int choix=-1;
+	while (choix != 0){
+		
+		//afficher le menu
+		printf("----------------------------------------------------------\n");
+		printf("1 : Afficher toutes les recettes dans l'ordre alphabetique\n");
+		printf("2 : Afficher une recette selon son nom\n");
+		printf("0 : Quitter le menu\n");
+		printf("----------------------------------------------------------\n");
+		printf("Quel est votre choix : ");
+		
+		scanf("%d", &choix);
+		getchar();		//obliger de mettre ce getchar pour le entree du scanf
+		
+		//efface le menu quand on a fait son choix
+		system("cls");
+		
+		switch(choix){
+			case 1 :
+				//on affiche les recettes dans l'ordre alphabetique tant que l'utilisateur ne tape pas m
+				while(temp1 != 'm'){
+					printf("Afficher toutes les recettes dans l'ordre alphabetique\n");
+					afficher_arbre_recette(n_r);
+					printf("\n");
+					
+					printf("saisir m pour revenir au menu\n");
+					scanf("%c", &temp1);
+					getchar();
+					
+					system("cls");	//on efface l'ecran et on reaffiche le menu si l'utilisateur n'a pas entrer m
+				}
+				temp1 = '\0';		//remet la var temp a "0"
+				system("cls");
+				break;
+			
+			case 2 :
+				while(temp1 != 'm'){
+					printf("Afficher une recette selon son nom\n");
+					printf("Quelle recette voulez vous voir ?");
+					temp2=lecture();
+					
+					afficher_recette(rechercher_recette(temp2, n_r));
+					
+					printf("saisir m pour revenir au menu\n");
+					scanf("%c", &temp1);
+					getchar();
+					
+					system("cls");
+				}
+				temp1 = '\0';
+				system("cls");
+				break;
+				
+			case 0 :
+				printf("Vous avez quitte le menu\n");
+				break;
+				
+			default :
+				printf("Erreur lors de la saisie, resaisir\n");
+		}
+	}
+}
---
+++ menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#include "gestion_ingredient.h"
+#include "gestion_recette.h"
+
+void menu(noeud_ingredient* n_i, noeud_recette* n_r);
+
+#endif // MENU_H_INCLUDED
